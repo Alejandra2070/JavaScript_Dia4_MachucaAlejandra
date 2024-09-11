ca76efc3105e2f921aeff8b27e9dc0f58033ee14
Alejandra2070 <am3793825@gmail.com>
2024-09-11 15:34:49 -0500

    :sparkles: Code fixes

--- Dia 12/script.js
+++ Dia 12/script.js
@@ -1,34 +1,74 @@
-document.getElementById("añadir").addEventListener("click", añadirCarta)
-/*document.getElementById("listo").addEventListener("click", contarCartas)*/
-var url="https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-mostrarUrl(url)
-function mostrarUrl(url){
+var url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+var total = 0;
+var deckId = "";
+
+mostrarUrl(url);
+
+function mostrarUrl(url) {
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        var otro = data.deck_id
-        var linkDos="https://deckofcardsapi.com/api/deck/"+String(otro)+"/draw/?count=2";
-
-        fetch(linkDos)
         .then(res => res.json())
-        .then(cartas=> {
-            /*console.log(cartas.cards[0].image)*/
-            let crear = document.getElementById("imgs");
-            crear.innerHTML=`
-            <img id="img1" src=${cartas.cards[0].image}>
-            `
+        .then(data => {
+            deckId = data.deck_id;
+            document.getElementById("buttonNew").disabled = false;
         });
-        
-    })
 }
-function añadirCarta(){
-    crear.innerHTML=`
-    <img id="img1" src=${cartas.cards[0].image}>
-    `
-}
-/*function añadirCarta(){
-    
-}
-function contarCartas(){
 
-}*/
+function nuevaCarta() {
+    var linkDos = "https://deckofcardsapi.com/api/deck/" + deckId + "/draw/?count=1";
+
+    fetch(linkDos)
+        .then(res => res.json())
+        .then(cartas => {
+            let crear = document.getElementById("imgs");
+            let img = document.createElement("img");
+            img.src = cartas.cards[0].image;
+            crear.appendChild(img);
+
+            total += calcularValor(cartas.cards[0].value);
+            let mensaje = document.getElementById("mensaje");
+
+            if (total === 21) {
+                mensaje.innerText = "¡Felicidadessss, acabas de ganar :D!";
+                document.getElementById("buttonNew").disabled = true;
+                document.getElementById("buttonReset").style.display="inline-block";
+            } 
+            else if (total > 21) {
+                mensaje.innerText = "Perdiste, te pasaste de 21 :(";
+                document.getElementById("buttonNew").disabled = true;
+                document.getElementById("buttonReset").style.display="inline-block";
+            } 
+            else {
+                mensaje.innerText = "Tu total es " + total + ". Sigue jugando.";
+            }
+        });
+}
+
+function calcularValor(valor) {
+    if (valor === "ACE") {
+        return 11;
+    }
+    else if (["KING", "QUEEN", "JACK"].includes(valor)) {
+        return 10;
+    } 
+    else {
+        return parseInt(valor);
+    }
+}
+
+function reintentarJuego() {
+    total = 0;
+    document.getElementById("imgs").innerHTML = "";
+    document.getElementById("mensaje").innerText = "";
+    document.getElementById("buttonNew").disabled = false;
+    document.getElementById("buttonReset").style.display = "none";
+    mostrarUrl(url);
+}
+
+function mostrarReglas(){
+    var reglas = document.getElementById("reglas");
+    if (reglas.style.display === "none") {
+        reglas.style.display = "block";
+    } else {
+        reglas.style.display = "none";
+    }
+}
